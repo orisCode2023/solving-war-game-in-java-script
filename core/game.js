8bf6555e07bbaaa5934d44d701b93c0ef9fb5a87
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-07 17:59:21 +0200

    unfiniish

--- core/game.js
+++ core/game.js
@@ -1,18 +1,18 @@
-import { deal } from "../helper-function/help.js"
+import { deal, isNotEmty, playerWin } from "../helper-function/help.js"
 import deck from "../utils/deck.js"
 import input from "analiza-sync"
 
 
 
 
-function createPlayer(name="AI"){
+function createPlayer(name = "AI") {
     return {
         name,
-        hand:[],
-        wonPile:[]
+        hand: [],
+        wonPile: []
     }
-} 
-export function initGame(){
+}
+export function initGame() {
     const name = input("Enter your name: ")
     const p1 = createPlayer()
     const p2 = createPlayer(name)
@@ -20,11 +20,68 @@ export function initGame(){
     deal(gameDeck, p1.hand, p2.hand)
     return {
         deck: gameDeck,
-        player_1 : p1,
-        player_2 : p2
+        player_1: p1,
+        player_2: p2
     }
 }
 
-export function playRound(p1, p2){
-     
-} 
+function checkResult(result, p1, p2, card1, card2) {
+    if (result === "p1") {
+        playerWin(p1.wonPile, card1, card2)
+    } else if (result === "p2") {
+        playerWin(p2.wonPile, card1, card2)
+    } else {
+        war(p1, p2, card1, card2)
+    }
+}
+
+function war(p1, p2, card1, card2) {
+    let counter = 0
+    const p1WarPile = []
+    const p2WarPile = []
+    p1WarPile.push(card1)
+    p2WarPile.push(card2)
+    while (counter <= 3) {
+        if (p1.hand){
+            card1 = p1.hand.pop()
+            p1WarPile.push(card1)
+        } 
+        if (p2.hand){
+            card2 = p2.hand.pop()
+            p2WarPile.push(card2)
+        }
+        counter++
+        const resultRound = deck.compareCards(card1, card2)
+        if (counter === 3 && resultRound === "WAR") {
+            counter = 0
+        } else {
+            checkResult(resultRound, p1, p2, card1, card2)
+        }
+    }
+}
+
+
+
+function playRound(p1, p2) {
+    let p1Card;
+    let p2Card;
+    if (p1.hand){
+        p1Card = p1.hand.pop()
+    }
+    if (p2.hand){
+        p2Card = p2.hand.pop()
+    }
+    console.log(`${p1.name} card is: ${p1Card.value} `)
+    console.log(`${p2.name} card is: ${p2Card.value} `)
+    let resultRound = deck.compareCards(p1Card, p2Card)
+    checkResult(resultRound, p1, p2, card1, card2)
+}
+const game = initGame()
+playRound(game.player_1, game.player_2)
+
+
+export function gameLoop() {
+    while (isNotEmty(p1.hand, p1.wonPile) || isNotEmty(p2.hand, p2.wonPile)) {
+        break
+    }
+}
--- helper-function/help.js
+++ helper-function/help.js
@@ -9,6 +9,13 @@ export const objSuites= {
 
 }
 export const deal = (deck, p1, p2) => {
-    p1.push(deck.slice(0, deck.length / 2))
-    p2.push(deck.slice(deck.length / 2))
+    p1.push(...deck.slice(0, deck.length / 2))
+    p2.push(...deck.slice(deck.length / 2))
 }
+
+export const playerWin = (player, card1, card2) => {
+    player.push(card1, card2)
+} 
+
+export const isNotEmty = (hand, pile) => hand.length > 0 && pile.hand > 0 
+export const reFillPile = (player) => player.hand.push(...player.wonPile)
