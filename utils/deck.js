a4c57dc768fcd71548fc131cff07222c68aaca53
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-07 15:07:22 +0200

    upgrading the create deck

--- utils/deck.js
+++ utils/deck.js
@@ -1,10 +1,10 @@
-import { converNumerTochar, convertCharToNumber } from "../helper-function/help.js"
+import { objSuites } from "../helper-function/help.js"
 
 function createCard(rank, suite) {
     return {
         rank,
         suite,
-        value: Number(convertCharToNumber(rank))
+        value: Number.isNaN(objSuites[rank]) ? objSuites.keys()[rank] : objSuites[rank]
     }
 }
 
@@ -21,23 +21,23 @@ function compareCards(p1Card, p2Card) {
 function createDeck() {
     const suitsDeck = ["H", "S", "D", "C"]
     const deck = []
-    for (let i = 0; i < suitsDeck.length; i++){
-        for (let j = 2; j <= 14; j++){
-            deck.push(createCard(converNumerTochar(j), suitsDeck[i]))
-        }
-    } 
-    return deck
-}
-
-function shuffle(deck) {
-    let shuffleTimes = 1000
-    while(shuffleTimes > 0){
-        let num1 = Math.floor(Math.random() * deck.length);
-        let num2 = Math.floor(Math.random() * deck.length);
-        if (num1 !== num2){
-            [deck[num1], deck[num2]] = [deck[num2], deck[num1]]
-            shuffleTimes--    
+    for (let i = 0; i < suitsDeck.length; i++) {
+        for (let j = 2; j <= 14; j++) {
+            deck.push(createCard(j, suitsDeck[i]))
         }
     }
     return deck
 }
+function shuffle(deck) {
+    let shuffleTimes = 1000
+    while (shuffleTimes > 0) {
+        let num1 = Math.floor(Math.random() * deck.length);
+        let num2 = Math.floor(Math.random() * deck.length);
+        if (num1 !== num2) {
+            [deck[num1], deck[num2]] = [deck[num2], deck[num1]]
+            shuffleTimes--
+        }
+    }
+    return deck
+}
+export default { shuffle, compareCards, createDeck }
