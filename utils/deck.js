208e62050193156617c3d12e64a69ec39527e7e1
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-07 11:37:03 +0200

    creating the deck fumction with importing helper function

--- utils/deck.js
+++ utils/deck.js
@@ -1,4 +1,4 @@
-import { convertCharToNumber } from "../helper function/help.js"
+import { converNumerTochar, convertCharToNumber } from "../helper function/help.js"
 
 function createCard(rank, suite) {
     return {
@@ -19,8 +19,16 @@ function compareCards(p1Card, p2Card) {
 }
 
 function createDeck() {
-    return [Object]
+    const suitsDeck = ["H", "S", "D", "C"]
+    const deck = []
+    for (let i = 0; i < suitsDeck.length; i++){
+        for (let j = 2; j <= 14; j++){
+            deck.push(createCard(converNumerTochar(j), suitsDeck[i]))
+        }
+    } 
+    return deck
 }
+
 
 function shuffle(deck) {
     return [Object]
