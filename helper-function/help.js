55faca1a3f4b9263eb4e4d27e50c8ec34c42e782
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-07 15:08:57 +0200

    deleting two converting functions and creating an object

--- helper-function/help.js
+++ helper-function/help.js
@@ -1,39 +1,13 @@
-export const convertCharToNumber = (char) => {
-    switch (char) {
-        case "J":
-            char = "11"
-            break;
-        case "Q":
-            char = "12"
-            break;
-        case "K":
-            char = "13"
-            break;
-        case "A":
-            char = "14"
-            break;    
-    }
-    return char
-}
 
-export const converNumerTochar = number =>{
-        switch (number) {
-        case 11:
-            number = "J"
-            break;
-        case 12:
-            number = "Q"
-            break;
-        case 13:
-            number = "K"
-            break;
-        case 14:
-            number = "A"
-            break;      
-    }
-    return number
-}
+const numbers={2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
+export const objSuites= {
+    14:"A",
+    13:"K",
+    12:"Q",
+    11:"J",
+    ...numbers
 
+}
 export const deal = (deck, p1, p2) => {
     p1.push(deck.slice(0, deck.length / 2))
     p2.push(deck.slice(deck.length / 2))
