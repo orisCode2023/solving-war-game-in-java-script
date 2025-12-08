38acac6f5a8fda3bddca3cbb2110536662bd817d
Ori <odmark2023@gmail.com>
2025-12-08 07:55:47 +0200

    function play round wirking without the adding cards to the winner but the function return who the winner is

--- core/game.js
+++ core/game.js
@@ -1,6 +1,6 @@
 import { deal, isNotEmty, playerWin } from "../helper-function/help.js"
 import deck from "../utils/deck.js"
-import input from "analiza-sync"
+// import input from "analiza-sync"
 
 
 
@@ -13,7 +13,8 @@ function createPlayer(name = "AI") {
     }
 }
 export function initGame() {
-    const name = input("Enter your name: ")
+    // const name = input("Enter your name: ")
+    const name = "David"
     const p1 = createPlayer()
     const p2 = createPlayer(name)
     const gameDeck = deck.shuffle(deck.createDeck())
@@ -25,40 +26,40 @@ export function initGame() {
     }
 }
 
-function checkResult(result, p1, p2, card1, card2) {
-    if (result === "p1") {
-        playerWin(p1.wonPile, card1, card2)
-    } else if (result === "p2") {
-        playerWin(p2.wonPile, card1, card2)
-    } else {
-        war(p1, p2, card1, card2)
-    }
-}
+// function checkResult(result, p1, p2, card1, card2) {
+//     if (result === "p1") {
+//         playerWin(p1.wonPile, card1, card2)
+//     } else if (result === "p2") {
+//         playerWin(p2.wonPile, card1, card2)
+//     } else {
+//         war(p1, p2, card1, card2)
+//     }
+// }
 
-function war(p1, p2, card1, card2) {
-    let counter = 0
-    const p1WarPile = []
-    const p2WarPile = []
-    p1WarPile.push(card1)
-    p2WarPile.push(card2)
-    while (counter <= 3) {
-        if (p1.hand){
-            card1 = p1.hand.pop()
-            p1WarPile.push(card1)
-        } 
-        if (p2.hand){
-            card2 = p2.hand.pop()
-            p2WarPile.push(card2)
-        }
-        counter++
-        const resultRound = deck.compareCards(card1, card2)
-        if (counter === 3 && resultRound === "WAR") {
-            counter = 0
-        } else {
-            checkResult(resultRound, p1, p2, card1, card2)
-        }
-    }
-}
+// function war(p1, p2, card1, card2) {
+//     let counter = 0
+//     const p1WarPile = []
+//     const p2WarPile = []
+//     p1WarPile.push(card1)
+//     p2WarPile.push(card2)
+//     while (counter <= 3) {
+//         if (p1.hand){
+//             card1 = p1.hand.pop()
+//             p1WarPile.push(card1)
+//         } 
+//         if (p2.hand){
+//             card2 = p2.hand.pop()
+//             p2WarPile.push(card2)
+//         }
+//         counter++
+//         const resultRound = deck.compareCards(card1, card2)
+//         if (counter === 3 && resultRound === "WAR") {
+//             counter = 0
+//         } else {
+//             checkResult(resultRound, p1, p2, card1, card2)
+//         }
+//     }
+// }
 
 
 
@@ -74,14 +75,15 @@ function playRound(p1, p2) {
     console.log(`${p1.name} card is: ${p1Card.value} `)
     console.log(`${p2.name} card is: ${p2Card.value} `)
     let resultRound = deck.compareCards(p1Card, p2Card)
-    checkResult(resultRound, p1, p2, card1, card2)
+    console.log(resultRound)
+    // checkResult(resultRound, p1, p2, card1, card2)
 }
 const game = initGame()
 playRound(game.player_1, game.player_2)
 
 
-export function gameLoop() {
-    while (isNotEmty(p1.hand, p1.wonPile) || isNotEmty(p2.hand, p2.wonPile)) {
-        break
-    }
-}
+// export function gameLoop() {
+//     while (isNotEmty(p1.hand, p1.wonPile) || isNotEmty(p2.hand, p2.wonPile)) {
+//         break
+//     }
+// }
